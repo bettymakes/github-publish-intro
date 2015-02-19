70f38a478cf9e1d0ab3299847f8607d3228c6dbd
blibotics <blibotics@gmail.com>
2015-02-19 16:09:43 -0500

    Added some filler steps and placeholder text, starting to style

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,4 +5,60 @@ var app = angular.module('githubApp', []);
 app.controller('defaultCtrl', function($scope) {
     console.log('er ma gerd fired');
     $scope.testing = "wow";
+
+//region Steps
+    $scope.currentStep = 0;
+    $scope.steps = [
+        {
+            title: 'First',
+            subtitle: 'only if',
+            image: "http://assets6.pokemon.com/assets/cms2/img/video-games/_tiles/pokemon-shuffle/pokemon-shuffle-169.jpg",
+            imageText: "haha"
+        },
+        {
+            title: 'Second',
+            subtitle: '',
+            image: "../images/test.jpg",
+            imageText: "wewe"
+        },
+        {
+            title: 'Third',
+            subtitle: 'this shows',
+            image: "http://assets18.pokemon.com/assets/cms2/img/trading-card-game/_tiles/xy05/xy05-groudon-kyogre-169.jpg",
+            imageText: "dada"
+        }
+    ];
+
+    $scope.nextStep = function() {
+        if (this.currentStep <= 10) {
+            this.currentStep++;
+            $scope.setProgressPercentage(this.currentStep);
+        }
+    };
+
+    $scope.previousStep = function() {
+        if (this.currentStep > 0) {
+            this.currentStep--;
+            $scope.setProgressPercentage(this.currentStep);
+        }
+    };
+//endregion
+
+//region Progress Bar
+    $scope.calcProgressPercentage = function(current, length){
+        var currentNum = current + 1,
+            percentage = Math.round((currentNum/length)*100);
+        return percentage
+    };
+
+    $scope.setProgressPercentage = function(current){
+        console.log('setProg current step', current);
+        $scope.progressPercentage = $scope.calcProgressPercentage(current, $scope.steps.length);
+    };
+
+    // Initialize Progress Bar Fill on load
+    $scope.setProgressPercentage($scope.currentStep);
+    $scope.isFirstStep = function(){};
+//endregion
+
 });
