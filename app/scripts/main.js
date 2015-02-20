f4f52d055a686d2074f2c4574a7756eda085adac
blibotics <blibotics@gmail.com>
2015-02-20 05:59:18 -0500

    Made changes to image paths to match server, changed logic to fix bug when developer tools is showing, moved tag's zindex to show above intro

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,13 +14,13 @@ app.controller('defaultCtrl', function($scope) {
         {
             title: 'Log into your <a href="https://github.com/login">Github</a> account.',
             subtitle: 'If you do not have a Github account, you\'ll need to <a href="https://github.com/join">create one</a> first.',
-            image: "../images/01-sign-in.jpg",
+            image: "../github-intro-to-publishing/images/01-sign-in.jpg",
             imageText: "Log into Github"
         },
         {
             title: 'Click on the plus sign icon in the navigation bar and select \'New repository\'.',
             subtitle: '',
-            image: "../images/02-create-new-repo.jpg",
+            image: "../github-intro-to-publishing/images/02-create-new-repo.jpg",
             imageText: "Create new repository"
         },
         {
@@ -28,37 +28,37 @@ app.controller('defaultCtrl', function($scope) {
             subtitle: 'Your repository name is essentially the name of your project. It should be relevant to what your making. ' +
                 '<br>\'Poop-Monster\' would be an example of a bad name for your repository (unless, your work is actually poop-monster related). ' +
                 '<br>Hey, whatever floats your boat, my friend!',
-            image: "../images/03-repo-name.jpg",
+            image: "../github-intro-to-publishing/images/03-repo-name.jpg",
             imageText: "Name new repo"
         },
         {
             title: "Select 'Initialize this repository with a README' and then 'Create repository'.",
             subtitle: 'Double check that \'Public\' is selected. It should already be chosen by default.',
-            image: "../images/04-public-readme-create.jpg",
+            image: "../github-intro-to-publishing/images/04-public-readme-create.jpg",
             imageText: "Create Repository with README "
         },
         {
             title: "Your repository is now created. To make a new file, click on the plus sign next to your repository's name.",
             subtitle: '',
-            image: "../images/05-create-new-file.jpg",
+            image: "../github-intro-to-publishing/images/05-create-new-file.jpg",
             imageText: "Create New File"
         },
         {
             title: "Name your file 'index.html'.",
             subtitle: 'Github will be looking for the index.html file, so be sure to name it exactly as per instructions.',
-            image: "../images/06-name-file.jpg",
+            image: "../github-intro-to-publishing/images/06-name-file.jpg",
             imageText: "Name File index.html"
         },
         {
             title: "Copy your code from Sublime or from whatever IDE/text editor that you're using.",
             subtitle: '',
-            image: "../images/07-copy-code.jpg",
+            image: "../github-intro-to-publishing/images/07-copy-code.jpg",
             imageText: "Copy Code From IDE"
         },
         {
             title: "Head back to your web browser, to the page where we named our file 'index.html', and paste your code in the text editor.",
             subtitle: '',
-            image: "../images/08-paste-code.jpg",
+            image: "../github-intro-to-publishing/images/08-paste-code.jpg",
             imageText: "Paste Code"
         },
         {
@@ -66,20 +66,20 @@ app.controller('defaultCtrl', function($scope) {
             subtitle: 'If you have additional files like a \'styles.css\' or a \'main.js\', repeat steps 5 through 9. <br><br>' +
                 'For step 6, instead of naming your file \'index.html\', you\'ll need to name those additional files accordingly. <br><br>' +
                 'If you\'re not too sure how to do that, fear not my friend! I\'m just a <a href="https://twitter.com/blibotics/">tweet</a> away.',
-            image: "../images/09-commit-file.jpg",
+            image: "../github-intro-to-publishing/images/09-commit-file.jpg",
             imageText: "Commit File"
         },
         {
             title: "Once you're back on the main repository page, click on the dropdown that says \'branch: master\'.<br> " +
                 "In the input field, create a new branch called 'gh-pages'.",
             subtitle: 'In order for Github to get your website up online, they\'ll be looking for the gh-pages branch. So be sure to name your branch exactly as per instructions.',
-            image: "../images/10-create-branch-gh-pages.jpg",
+            image: "../github-intro-to-publishing/images/10-create-branch-gh-pages.jpg",
             imageText: "Create Branch gh-pages"
         },
         {
             title: "Et voila! That's it, you\'re done.",
             subtitle: 'Go grab a cup of coffee, it may take 5-10 minutes for your page to go live. I\'d love to check out your hard work. <a href="https://twitter.com/blibotics/">Share</a> it with me!',
-            image: "../images/11-its-alive.jpg",
+            image: "../github-intro-to-publishing/images/11-its-alive.jpg",
             imageText: ""
         }
     ];
@@ -107,7 +107,7 @@ app.controller('defaultCtrl', function($scope) {
     };
 
     $scope.setProgressPercentage = function(current){
-        console.log('setProg current step', current);
+//        console.log('setProg current step', current);
         $scope.progressPercentage = $scope.calcProgressPercentage(current, $scope.steps.length);
     };
 
