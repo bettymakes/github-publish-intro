3d8a7a75164ab2e56b0c74dba88051b7d2ce9d7f
blibotics <blibotics@gmail.com>
2015-02-20 05:26:43 -0500

    Fixed bug where the steps started at step2, updated copy for all steps

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,9 +3,11 @@
 var app = angular.module('githubApp', ['ngSanitize']);
 
 app.controller('defaultCtrl', function($scope) {
-    console.log('er ma gerd fired');
-    $scope.testing = "wow";
+    $scope.showIntroPage = true;
 
+    $scope.startTutorial = function(){
+        $scope.showIntroPage = false;
+    };
 //region Steps
     $scope.currentStep = 0;
     $scope.steps = [
@@ -16,62 +18,67 @@ app.controller('defaultCtrl', function($scope) {
             imageText: "Log into Github"
         },
         {
-            title: 'In the nav bar, you\'ll see a plus-sign icon. Click on that icon and select create new repository.',
+            title: 'Click on the plus sign icon in the navigation bar and select \'New repository\'.',
             subtitle: '',
             image: "../images/02-create-new-repo.jpg",
             imageText: "Create new repository"
         },
         {
             title: 'Enter a repository name',
-            subtitle: 'Think of this as the name of your project. It should be relevant and memorable.',
+            subtitle: 'Your repository name is essentially the name of your project. It should be relevant to what your making. ' +
+                '<br>\'Poop-Monster\' would be an example of a bad name for your repository (unless, your work is actually poop-monster related). ' +
+                '<br>Hey, whatever floats your boat, my friend!',
             image: "../images/03-repo-name.jpg",
             imageText: "Name new repo"
         },
         {
-            title: "Select initialize this repository with a README and create a repository.",
-            subtitle: 'Double check that Public is selected. It should already be chosen by default.',
+            title: "Select 'Initialize this repository with a README' and then 'Create repository'.",
+            subtitle: 'Double check that \'Public\' is selected. It should already be chosen by default.',
             image: "../images/04-public-readme-create.jpg",
             imageText: "Create Repository with README "
         },
         {
-            title: "To create a new file, click on the plus-sign next to your repository name.",
+            title: "Your repository is now created. To make a new file, click on the plus sign next to your repository's name.",
             subtitle: '',
             image: "../images/05-create-new-file.jpg",
             imageText: "Create New File"
         },
         {
             title: "Name your file 'index.html'.",
-            subtitle: '',
+            subtitle: 'Github will be looking for the index.html file, so be sure to name it exactly as per instructions.',
             image: "../images/06-name-file.jpg",
             imageText: "Name File index.html"
         },
         {
-            title: "Copy your code from Sublime or from whatever IDE you're using",
+            title: "Copy your code from Sublime or from whatever IDE/text editor that you're using.",
             subtitle: '',
             image: "../images/07-copy-code.jpg",
             imageText: "Copy Code From IDE"
         },
         {
-            title: "Go back to your web browser to the page where we named our file 'index.html' and paste your code.",
+            title: "Head back to your web browser, to the page where we named our file 'index.html', and paste your code in the text editor.",
             subtitle: '',
             image: "../images/08-paste-code.jpg",
             imageText: "Paste Code"
         },
         {
             title: "Click on the 'Commit new file' button at the bottom of the page to save your changes.",
-            subtitle: '',
+            subtitle: 'If you have additional files like a \'styles.css\' or a \'main.js\', repeat steps 5 through 9. <br><br>' +
+                'For step 6, instead of naming your file \'index.html\', you\'ll need to name those additional files accordingly. <br><br>' +
+                'If you\'re not too sure how to do that, fear not my friend! I\'m just a <a href="https://twitter.com/blibotics/">tweet</a> away.',
             image: "../images/09-commit-file.jpg",
             imageText: "Commit File"
         },
         {
-            title: "Back on your main repository page, click on the button that says 'branch: master' and in the input, create a new branch called 'gh-pages'.",
-            subtitle: 'Make sure your new branch is the exact same as the instructions \'gh-pages\'',
+            title: "Once you're back on the main repository page, click on the dropdown that says \'branch: master\'.<br> " +
+                "In the input field, create a new branch called 'gh-pages'.",
+            subtitle: 'In order for Github to get your website up online, they\'ll be looking for the gh-pages branch. So be sure to name your branch exactly as per instructions.',
             image: "../images/10-create-branch-gh-pages.jpg",
             imageText: "Create Branch gh-pages"
         },
         {
-            title: "And that's it! You're done. <br> <br> It may take 5-10 minutes for your page to go live.",
-            subtitle: '',
+            title: "Et voila! That's it, you\'re done.",
+            subtitle: 'Go grab a cup of coffee, it may take 5-10 minutes for your page to go live. I\'d love to check out your hard work. <a href="https://twitter.com/blibotics/">Share</a> it with me!',
             image: "../images/11-its-alive.jpg",
             imageText: ""
         }
